31a00363ebe8df270ed6f49f7970ee509642b50c
Bodanor <papadopoulos.chris@icloud.com>
2023-03-08 23:20:08 +0100

    Testing purposes

--- kernel/main.c
+++ kernel/main.c
@@ -4,8 +4,12 @@
 #include "screen.h"
 #include "ports.h"
 #include "timer.h"
+#include "rtc.h"
 
 #define DEBUG __asm__("xchgw %bx, %bx")
+
+extern rtc current_rtc;
+
 void kmain()__attribute__((__section__(".kmain")));
 
 void kmain() 
@@ -14,7 +18,9 @@ void kmain()
 	k_printf("<0> Booting succeeded\n");
 	isr_install();
 	irq_install();
+	read_rtc();
 	k_print_string(BLACK, CYAN, "Welcome to the kernel !\n", -1, -1);
+	k_printf("<2> TimeStamp : %d/%d/%d %d:%d:%d\n",current_rtc.day, current_rtc.month, current_rtc.year, current_rtc.hour, current_rtc.minute, current_rtc.second);
 	wait_timer(1000);
 	init_shell();
 	shell_loop();
