647067a6afe3715fc80f9ae0b920ed21be72a08d
Bodanor <papadopoulos.chris@icloud.com>
2023-03-08 17:08:50 +0100

    The instructions of the kmain function should always be the first ones in the binary

--- kernel/main.c
+++ kernel/main.c
@@ -1,15 +1,18 @@
 #include <stdint.h>
+#include "shell.h"
 #include "isr.h"
 #include "screen.h"
 #include "ports.h"
 
 #define DEBUG __asm__("xchgw %bx, %bx")
+void kmain()__attribute__((__section__(".kmain")));
 
-void main()
+void kmain() 
 {
 	k_print_string(BLACK, WHITE, "Welcome to the Kernel\n", -1, -1);
 	k_print_log(SUCCESS,"Booting succeded\n");
 	isr_install();
 	irq_install();
-	while (1);	
+	init_shell();
+	shell_loop();
 }
