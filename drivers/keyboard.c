0f8fbcc7b59a7b982db21ca49c3d0778caf519d3
Bodanor <papadopoulos.chris@icloud.com>
2023-03-08 17:07:42 +0100

    Input is directly available

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -4,18 +4,12 @@
 #include "screen.h"
 #include "string.h"
 
-#define BACKSPACE 0x0E
-#define ENTER 0X1C
 #define SC_MAX 57
 
-static char key_buffer[256];
+static char current_letter;
+static uint32_t current_keymap;
+volatile static int new_input;
 
-const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
-        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
-        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
-        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
 const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
@@ -26,16 +20,34 @@ const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',
 void keyboard_callback(registers *regs)
 {
 	uint8_t scancode = port_byte_in(0x60);
-	if (scancode > SC_MAX) return;
-
+	if (scancode > SC_MAX)
+		return;
+	
+	else if (scancode == ENTER)
+		current_letter = ENTER;
 	else {
-		char letter = sc_ascii[(int)scancode];
-		k_print_chr(BLACK, WHITE, letter, -1, -1);
+		current_letter = sc_ascii[(int)scancode];
 	}
-
+	new_input = 1;
 
 }
 void init_keyboard()
 {
 	register_int_handler(IRQ1, keyboard_callback);
+	current_keymap = 0;
+	new_input = 0;
+}
+
+char getLastInput(void)
+{
+	while (new_input == 0)
+		;
+
+	new_input = 0;
+	return current_letter;
+}
+
+void set_keymap(uint32_t keymap)
+{
+	current_keymap = keymap;
 }
--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,5 +1,16 @@
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
 
+#include <stdint.h>
+
+#define US_KEYMAP 0
+#define BE_KEYMAP 1
+#define FR_KEYMAP 2
+
+#define BACKSPACE 0x0E
+#define ENTER 0X1C
+
 void init_keyboard(void);
+char getLastInput(void);
+void set_keymap(uint32_t keymap);
 #endif
